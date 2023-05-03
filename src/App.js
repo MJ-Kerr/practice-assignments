10d61345900f5a900ee530ea3f8bcfade675d16b
Miles Kerr <m.kerr.professional@gmail.com>
2023-05-03 01:25:39 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// const PersonCard = (props) => {
+//   return (
+//     <div>
+//       <h1>{props.lastName}, {props.firstName}</h1>
+//       <p>Age: {props.age}</p>
+//       <p>Hair Color: {props.hairColor}</p>
+//     </div>
+//   )
+// }
+// //Note: we can pass down a string with or without curly braces.
+// <PersonCard firstName="John" lastName="Smith" age={8} hairColor="Brown" />
 
-export default App;
+// export default PersonCard;
